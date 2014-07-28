3c4b236e684ab49580f0db60e86cfebc0788c7d7
Kumar Bala <skb@localhost.localdomain>
2014-07-28 22:53:40 +0100

    sockit: First bootting version!

--- conf/board/sockit/minifs-script.sh
+++ conf/board/sockit/minifs-script.sh
@@ -3,18 +3,12 @@
 
 TARGET_LIBC=eglibc
 
-TARGET_META_ARCH=armv7
-TARGET_ARCH=arm
-TARGET_FULL_ARCH=$TARGET_ARCH-a9-linux-gnueabi
-TARGET_KERNEL_NAME=uImage
-TARGET_LIBC_CFLAGS="-g -O2 -march=armv7-a -mtune=cortex-a9 -mfpu=neon -fPIC -mthumb-interwork"
-TARGET_CFLAGS="$TARGET_LIBC_CFLAGS"
 # this file .dts must exist either in this directory (board)
 # or in the linux arch/$TARGET_ARCH/boot/dts/
 TARGET_KERNEL_DTB=${TARGET_KERNEL_DTB:-socfpga_cyclone5_sockit}
 
-TARGET_FS_EXT=0
-#TARGET_FS_TAR=0
+TARGET_FS_EXT=1
+TARGET_FS_TAR=1
 TARGET_FS_SQUASH=1
 
 board_set_versions() {
@@ -42,6 +36,12 @@ board_prepare() {
 	export ROOTFS_KEEPERS
 
 }
+
+#
+# Create a ramdisk image with just busybox and a few tools, this
+# mini ramdisk is responsible for finding were the real linux distro is
+# mount it and start it
+#
 sockit-setup-initrd() {
 	mkdir -p $BUILD/initramfs
 	rm -rf $BUILD/initramfs/*
@@ -64,6 +64,7 @@ sockit-setup-initrd() {
 	)>$BUILD/._initramfs.log
 	(
 		cd $BUILD/initramfs/
+		## Add rootfs overrides for boards
 		for pd in $(minifs_locate_config_path initramfs 1); do
 			if [ -d "$pd" ]; then
 				echo "### Installing initramfs $pd"
